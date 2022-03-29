5b832dbda3fc91ffca328fd20d6ec8a355dfd0a7
LuTuanAnh188 <102581270+LuTuanAnh188@users.noreply.github.com>
2022-03-29 17:53:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    titleWeb: "Chúc Mừng Sinh Nhật Bé Iu",
+    introTitle: 'Mỹ Duyên',
+    introDesc: `Happy
+    Birthday
+    To
+    My Love ,
+    btnIntro: 'Iu Em',
+    title: 'Năm đầu tiên chúng ta đón sinh nhật với nhau',
+    desc: 'Em có vui khom?',
+    btnYes: 'Vui lắm ❤️',
+    btnNo: 'Hong bé ơi!!',
+    question: 'Ngày mai 10h em sẽ sang nhà anh để mình cùng đón sn chứ?',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vâng ngày mai em sẽ qua',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Thế nhé hẹn bé 10h sáng mai.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://m.me/LuTuanAnh1808' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
